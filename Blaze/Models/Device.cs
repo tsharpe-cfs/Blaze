81b4606d9d8bfcfff5b6d553fa65e921db51e3d9
tsharpe-cfs <140429050+tsharpe-cfs@users.noreply.github.com>
2024-02-04 13:51:21 -0500

    devices working

--- Blaze/Models/Device.cs
+++ Blaze/Models/Device.cs
@@ -2,6 +2,8 @@
 {
 	internal class Device
 	{
+		//NOTE(tsharpe): This is also the devices key, dupe?
+		public string Name { get; set; }
 		public DateTime LastSeen { get; set; }
 		public object Value { get; set; }
 	}
